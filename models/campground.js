093a0eb3e31c2b0c5fb9aa0e9e8ea2d595bb99d9
Michael Cohen <macball2000@yahoo.com>
2022-12-29 13:20:45 -0500

    Last Modified Timestamps, Styles, Images

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const campgroundSchema = new mongoose.Schema({
-  title: String,
-  image: String,
-  price: Number,
-  description: String,
-  location: String,
-});
+const campgroundSchema = new mongoose.Schema(
+  {
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Campground", campgroundSchema);
