f514d31097e1b90dbd3f7e06ac1279f50cfc1613
Michael Cohen <macball2000@yahoo.com>
2022-12-31 00:35:48 -0500

    Restructuring

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const methodOverride = require("method-override");
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/expressError");
 
 const Campground = require("../models/campground");
-const Review = require("../models/review");
-const Joi = require("joi");
-const { campgroundSchema, reviewSchema } = require("../schemas");
+const { campgroundSchema } = require("../schemas");
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
@@ -18,17 +15,6 @@ const validateCampground = (req, res, next) => {
     next();
   }
 };
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
-router.use(methodOverride("_method"));
 
 router.get(
   "/",
@@ -83,29 +69,6 @@ router.delete(
     const { id } = req.params;
     const deletedCampground = await Campground.findByIdAndDelete(id);
     res.redirect(`/campgrounds`);
-  })
-);
-router.post(
-  "/:id/reviews",
-  validateReview,
-  catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-router.delete(
-  "/:id/reviews/:reviewId",
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findById(reviewId);
-    res.redirect(`/campgrounds/${id}`);
   })
 );
 
